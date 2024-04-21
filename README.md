67750a1a3261c46dc938cee94f7a2b25d6db493e
Alex <110390382+Fialex1212@users.noreply.github.com>
2024-04-21 03:12:41 +0300

    2.0v

---
+++ README.md
@@ -0,0 +1,7 @@
+echo "# django-quiz_test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Fialex1212/django-quiz_test.git
+git push -u origin main
--- quiz_project/quiz_app/models.py
+++ quiz_project/quiz_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Quiz(models.Model):
     title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     class Meta:
         verbose_name_plural = "Quizzes"
@@ -24,7 +26,8 @@ class Question(models.Model):
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    content = models.TextField()
+    content = models.TextField(blank=True, null=True)
+    content_image = models.ImageField(upload_to='answer_images/', blank=True, null=True)
     is_correct = models.BooleanField(default=False)
 
     class Meta:
@@ -32,3 +35,14 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.content
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Answers"
+
+    def __str__(self):
+        return self.user.username
